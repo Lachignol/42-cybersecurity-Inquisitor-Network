ebaa21c7965a3bdc6da4e86d362f5baf98e8efeb
La chignol <ton.email@example.com>
2026-03-03 18:07:53 +0100

    batman begin

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "github.com/google/gopacket/pcap"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	var ip_src string
@@ -11,6 +16,14 @@ func main() {
 	if !setup(&ip_src, &mac_src, &ip_target, &mac_target) {
 		return
 	}
-	fmt.Println("args ok")
+	fmt.Println("VERIFICATION OF ARGS OK BRODY!")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	fmt.Println("Poisoning")
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+	fmt.Println("ARP TABLE RESTAURATION:", s)
 
 }
