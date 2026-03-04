ad8337677400cbef86a8cb9d193abd2779ac1246
La chignol <ton.email@example.com>
2026-03-04 18:45:59 +0100

    setup docker in progress

--- main.go
+++ main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
-	// "github.com/google/gopacket/pcap"
 	"os"
 	"os/signal"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 func main() {
-	var ip_src string
-	var mac_src string
-	var ip_target string
-	var mac_target string
+	// var ip_src string
+	// var mac_src string
+	// var ip_target string
+	// var mac_target string
 
-	if !setup(&ip_src, &mac_src, &ip_target, &mac_target) {
-		return
-	}
-	fmt.Println("VERIFICATION OF ARGS OK BRODY!")
+	// if !setup(&ip_src, &mac_src, &ip_target, &mac_target) {
+	// 	return
+	// }
+	// fmt.Println("VERIFICATION OF ARGS OK BRODY!")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	fmt.Println("Poisoning")
+	if handle, err := pcap.OpenLive("eth0", 1600, true, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter("arp"); err != nil { // optional
+		panic(err)
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			fmt.Print(packet)
+			// packet.ApplicationLayer == layers.ARPReply
+			// packet.ApplicationLayer == layers.ARPReply
+
+			// handlePacket(packet)
+		}
+	}
 
 	// Block until a signal is received.
 	s := <-c
